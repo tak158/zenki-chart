b257406ec905129fb58beff02756a171ab7645e0
Takao Uchikawa <158rune@gmail.com>
2014-01-03 12:19:10 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZenkiChart::Application.config.secret_key_base = 'e6afcddcec8671037439a397fc181a9548ca569cd40fcd855d3db522f00b4de9ac1346a15d03c4aa39e5621614baedb7179be7503324ebec542f4ce0546c93ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZenkiChart::Application.config.session_store :cookie_store, key: '_zenki-chart_session'
